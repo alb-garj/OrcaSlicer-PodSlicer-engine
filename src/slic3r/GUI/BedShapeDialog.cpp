alb-garj/OrcaSlicer-PodSlicer-engine

commit 543b7228c3076b6992e928c54b5838f11fc28727

New BuildVolume class was created, which detects build volume type (rectangular, circular, convex, concave) and performs efficient collision detection agains these build volumes. As of now, collision detection is performed against a convex hull of a concave build volume for efficency.
GCodeProcessor::Result renamed out of GCodeProcessor to GCodeProcessorResult,
so it could be forward declared.

Plater newly exports BuildVolume, not Bed3D. Bed3D is a rendering class,
while BuildVolume is a purely geometric class.

Reduced usage of global wxGetApp, the Bed3D is passed as a parameter
to View3D/Preview/GLCanvas.

Convex hull code was extracted from Geometry.cpp/hpp to Geometry/ConvexHulll.cpp,hpp.
New test inside_convex_polygon().
New efficent point inside polygon test: Decompose convex hull
to bottom / top parts and use the decomposition to detect point inside
a convex polygon in O(log n). decompose_convex_polygon_top_bottom(),
inside_convex_polygon().

New Circle constructing functions: circle_ransac() and circle_taubin_newton().

New polygon_is_convex() test with unit tests.

src/slic3r/GUI/BedShapeDialog.cpp
@@ -22,98 +22,7 @@ namespace GUI {
 
 BedShape::BedShape(const ConfigOptionPoints& points)
 {
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    if (points.size() < 3) {
-        m_type = Bed3D::EShapeType::Invalid;
-        return;
-    }
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-
-    // is this a rectangle ?
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    Vec2d min;
-    Vec2d max;
-    if (Bed3D::is_rectangle(points.values, &min, &max)) {
-        m_type       = Bed3D::EShapeType::Rectangle;
-        m_rectSize   = max - min;
-        m_rectOrigin = -min;
-        return;
-    }
-#else
-    Polygon polygon = Polygon::new_scale(points.values);
-    if (points.size() == 4) {
-        auto lines = polygon.lines();
-        if (lines[0].parallel_to(lines[2]) && lines[1].parallel_to(lines[3])) {
-            // okay, it's a rectangle
-            // find origin
-            coordf_t x_min, x_max, y_min, y_max;
-            x_max = x_min = points.values[0](0);
-            y_max = y_min = points.values[0](1);
-            for (auto pt : points.values)
-            {
-                x_min = std::min(x_min, pt(0));
-                x_max = std::max(x_max, pt(0));
-                y_min = std::min(y_min, pt(1));
-                y_max = std::max(y_max, pt(1));
-            }
-
-            m_type          = Type::Rectangular;
-            m_rectSize      = Vec2d(x_max - x_min, y_max - y_min);
-            m_rectOrigin    = Vec2d(-x_min, -y_min);
-
-            return;
-        }
-    }
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-
-    // is this a circle ?
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    Vec2d center;
-    double radius;
-    if (Bed3D::is_circle(points.values, &center, &radius)) {
-        m_type     = Bed3D::EShapeType::Circle;
-        m_diameter = 2.0 * radius;
-        return;
-    }
-
-    // This is a custom bed shape, use the polygon provided.
-    m_type = Bed3D::EShapeType::Custom;
-#else
-    {
-        // Analyze the array of points.Do they reside on a circle ?
-        auto center = polygon.bounding_box().center();
-        std::vector<double> vertex_distances;
-        double avg_dist = 0;
-        for (auto pt : polygon.points)
-        {
-            double distance = (pt - center).cast<double>().norm();
-            vertex_distances.push_back(distance);
-            avg_dist += distance;
-        }
-
-        avg_dist /= vertex_distances.size();
-        bool defined_value = true;
-        for (auto el : vertex_distances)
-        {
-            if (abs(el - avg_dist) > 10 * SCALED_EPSILON)
-                defined_value = false;
-            break;
-        }
-        if (defined_value) {
-            // all vertices are equidistant to center
-            m_type      = Type::Circular;
-            m_diameter  = unscale<double>(avg_dist * 2);
-
-            return;
-        }
-    }
-
-    if (points.size() < 3)
-        return;
-
-    // This is a custom bed shape, use the polygon provided.
-    m_type = Type::Custom;
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    m_build_volume = { points.values, 0. };
 }
 
 static std::string get_option_label(BedShape::Parameter param)
@@ -122,119 +31,101 @@ static std::string get_option_label(BedShape::Parameter param)
     case BedShape::Parameter::RectSize  : return L("Size");
     case BedShape::Parameter::RectOrigin: return L("Origin");
     case BedShape::Parameter::Diameter  : return L("Diameter");
-    default:                              return "";
+    default:                              assert(false); return {};
     }
 }
 
 void BedShape::append_option_line(ConfigOptionsGroupShp optgroup, Parameter param)
 {
     ConfigOptionDef def;
-
-    if (param == Parameter::RectSize) {
+    t_config_option_key key;
+    switch (param) {
+    case Parameter::RectSize:
         def.type = coPoints;
         def.set_default_value(new ConfigOptionPoints{ Vec2d(200, 200) });
         def.min = 0;
         def.max = 1200;
         def.label = get_option_label(param);
         def.tooltip = L("Size in X and Y of the rectangular plate.");
-
-        Option option(def, "rect_size");
-        optgroup->append_single_option_line(option);
-    }
-    else if (param == Parameter::RectOrigin) {
+        key = "rect_size";
+        break;
+    case Parameter::RectOrigin:
         def.type = coPoints;
         def.set_default_value(new ConfigOptionPoints{ Vec2d(0, 0) });
         def.min = -600;
         def.max = 600;
         def.label = get_option_label(param);
         def.tooltip = L("Distance of the 0,0 G-code coordinate from the front left corner of the rectangle.");
-        
-        Option option(def, "rect_origin");
-        optgroup->append_single_option_line(option);
-    }
-    else if (param == Parameter::Diameter) {
+        key = "rect_origin";
+        break;
+    case Parameter::Diameter:
         def.type = coFloat;
         def.set_default_value(new ConfigOptionFloat(200));
         def.sidetext = L("mm");
         def.label = get_option_label(param);
         def.tooltip = L("Diameter of the print bed. It is assumed that origin (0,0) is located in the center.");
-
-        Option option(def, "diameter");
-        optgroup->append_single_option_line(option);
+        key = "diameter";
+        break;
+    default:
+        assert(false);
     }
+
+    optgroup->append_single_option_line({ def, std::move(key) });
 }
 
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-wxString BedShape::get_name(Bed3D::EShapeType type)
+wxString BedShape::get_name(PageType type)
 {
     switch (type) {
-    case Bed3D::EShapeType::Rectangle: { return _L("Rectangular"); }
-    case Bed3D::EShapeType::Circle:    { return _L("Circular"); }
-    case Bed3D::EShapeType::Custom:    { return _L("Custom"); }
-    case Bed3D::EShapeType::Invalid:
-    default: return _L("Invalid");
+    case PageType::Rectangle:  return _L("Rectangular");
+    case PageType::Circle:     return _L("Circular");
+    case PageType::Custom:     return _L("Custom");
     }
+    // make visual studio happy
+    assert(false);
+    return {};
 }
-#else
-wxString BedShape::get_name(Type type)
-{
-    switch (type) {
-    case Type::Rectangular: return _L("Rectangular");
-    case Type::Circular: return _L("Circular");
-    case Type::Custom: return _L("Custom");
-    case Type::Invalid:
-    default: return _L("Invalid");
-    }
-}
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
 
-size_t BedShape::get_type()
+BedShape::PageType BedShape::get_page_type()
 {
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    return static_cast<size_t>(m_type == Bed3D::EShapeType::Invalid ? Bed3D::EShapeType::Rectangle : m_type);
-#else
-    return static_cast<size_t>(m_type == Type::Invalid ? Type::Rectangular : m_type);
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    switch (m_build_volume.type()) {
+    case BuildVolume::Type::Rectangle:
+    case BuildVolume::Type::Invalid:    return PageType::Rectangle;
+    case BuildVolume::Type::Circle:     return PageType::Circle;
+    case BuildVolume::Type::Convex:
+    case BuildVolume::Type::Custom:     return PageType::Custom;
+    }
+    // make visual studio happy
+    assert(false);
+    return PageType::Rectangle;
 }
 
 wxString BedShape::get_full_name_with_params()
 {
-    wxString out = _L("Shape") + ": " + get_name(m_type);
-
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    if (m_type == Bed3D::EShapeType::Rectangle) {
-#else
-    if (m_type == Type::Rectangular) {
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-        out += "\n" + _(get_option_label(Parameter::RectSize))  + ": [" + ConfigOptionPoint(m_rectSize).serialize()     + "]";
-        out += "\n" + _(get_option_label(Parameter::RectOrigin))+ ": [" + ConfigOptionPoint(m_rectOrigin).serialize()   + "]";
+    wxString out = _L("Shape") + ": " + get_name(this->get_page_type());
+    switch (m_build_volume.type()) {
+    case BuildVolume::Type::Circle:
+        out += "\n" + _L(get_option_label(Parameter::Diameter)) + ": [" + double_to_string(2. * unscaled<double>(m_build_volume.circle().radius)) + "]";
+        break;
+    default:
+        // rectangle, convex, concave...
+        out += "\n" + _(get_option_label(Parameter::RectSize)) + ": [" + ConfigOptionPoint(to_2d(m_build_volume.bounding_volume().size())).serialize() + "]";
+        out += "\n" + _(get_option_label(Parameter::RectOrigin)) + ": [" + ConfigOptionPoint(to_2d(m_build_volume.bounding_volume().min)).serialize() + "]";
+        break;
     }
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    else if (m_type == Bed3D::EShapeType::Circle)
-#else
-    else if (m_type == Type::Circular)
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-        out += "\n" + _L(get_option_label(Parameter::Diameter)) + ": [" + double_to_string(m_diameter)                  + "]";
-
     return out;
 }
 
 void BedShape::apply_optgroup_values(ConfigOptionsGroupShp optgroup)
 {
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    if (m_type == Bed3D::EShapeType::Rectangle || m_type == Bed3D::EShapeType::Invalid) {
-#else
-    if (m_type == Type::Rectangular || m_type == Type::Invalid) {
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-        optgroup->set_value("rect_size"     , new ConfigOptionPoints{ m_rectSize    });
-        optgroup->set_value("rect_origin"   , new ConfigOptionPoints{ m_rectOrigin  });
+    switch (m_build_volume.type()) {
+    case BuildVolume::Type::Circle:
+        optgroup->set_value("diameter", double_to_string(2. * unscaled<double>(m_build_volume.circle().radius)));
+        break;
+    default:
+        // rectangle, convex, concave...
+        optgroup->set_value("rect_size"     , new ConfigOptionPoints{ to_2d(m_build_volume.bounding_volume().size()) });
+        optgroup->set_value("rect_origin"   , new ConfigOptionPoints{ to_2d(m_build_volume.bounding_volume().min) });
     }
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    else if (m_type == Bed3D::EShapeType::Circle)
-#else
-    else if (m_type == Type::Circular)
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-        optgroup->set_value("diameter", double_to_string(m_diameter));
 }
 
 void BedShapeDialog::build_dialog(const ConfigOptionPoints& default_pt, const ConfigOptionString& custom_texture, const ConfigOptionString& custom_model)
@@ -295,28 +186,16 @@ void BedShapePanel::build_panel(const ConfigOptionPoints& default_pt, const Conf
 
     sbsizer->Add(m_shape_options_book);
 
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    auto optgroup = init_shape_options_page(BedShape::get_name(Bed3D::EShapeType::Rectangle));
-#else
-    auto optgroup = init_shape_options_page(BedShape::get_name(BedShape::Type::Rectangular));
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    auto optgroup = init_shape_options_page(BedShape::get_name(BedShape::PageType::Rectangle));
     BedShape::append_option_line(optgroup, BedShape::Parameter::RectSize);
     BedShape::append_option_line(optgroup, BedShape::Parameter::RectOrigin);
     activate_options_page(optgroup);
 
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    optgroup = init_shape_options_page(BedShape::get_name(Bed3D::EShapeType::Circle));
-#else
-    optgroup = init_shape_options_page(BedShape::get_name(BedShape::Type::Circular));
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    optgroup = init_shape_options_page(BedShape::get_name(BedShape::PageType::Circle));
     BedShape::append_option_line(optgroup, BedShape::Parameter::Diameter);
     activate_options_page(optgroup);
 
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    optgroup = init_shape_options_page(BedShape::get_name(Bed3D::EShapeType::Custom));
-#else
-    optgroup = init_shape_options_page(BedShape::get_name(BedShape::Type::Custom));
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    optgroup = init_shape_options_page(BedShape::get_name(BedShape::PageType::Custom));
 
 	Line line{ "", "" };
 	line.full_width = 1;
@@ -538,8 +417,8 @@ void BedShapePanel::set_shape(const ConfigOptionPoints& points)
 {
     BedShape shape(points);
 
-    m_shape_options_book->SetSelection(shape.get_type());
-    shape.apply_optgroup_values(m_optgroups[shape.get_type()]);
+    m_shape_options_book->SetSelection(int(shape.get_page_type()));
+    shape.apply_optgroup_values(m_optgroups[int(shape.get_page_type())]);
 
     // Copy the polygon to the canvas, make a copy of the array, if custom shape is selected
     if (shape.is_custom())
@@ -562,17 +441,9 @@ void BedShapePanel::update_shape()
 	auto page_idx = m_shape_options_book->GetSelection();
     auto opt_group = m_optgroups[page_idx];
 
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    Bed3D::EShapeType page_type = static_cast<Bed3D::EShapeType>(page_idx);
-#else
-    BedShape::Type page_type = static_cast<BedShape::Type>(page_idx);
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    if (page_type == Bed3D::EShapeType::Rectangle) {
-#else
-    if (page_type == BedShape::Type::Rectangular) {
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    switch (static_cast<BedShape::PageType>(page_idx)) {
+    case BedShape::PageType::Rectangle:
+    {
         Vec2d rect_size(Vec2d::Zero());
 		Vec2d rect_origin(Vec2d::Zero());
 
@@ -602,12 +473,10 @@ void BedShapePanel::update_shape()
                     Vec2d(x1, y0),
                     Vec2d(x1, y1),
                     Vec2d(x0, y1) };
+        break;
     }
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    else if (page_type == Bed3D::EShapeType::Circle) {
-#else
-    else if (page_type == BedShape::Type::Circular) {
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    case BedShape::PageType::Circle:
+    {
         double diameter;
 		try { diameter = boost::any_cast<double>(opt_group->get_value("diameter")); }
 		catch (const std::exception & /* e */) { return; } 
@@ -615,6 +484,7 @@ void BedShapePanel::update_shape()
  		if (diameter == 0.0) return ;
 		auto r = diameter / 2;
 		auto twopi = 2 * PI;
+        // Don't change this value without adjusting BuildVolume constructor detecting circle diameter!
         auto edges = 72;
         std::vector<Vec2d> points;
         for (int i = 1; i <= edges; ++i) {
@@ -622,13 +492,12 @@ void BedShapePanel::update_shape()
 			points.push_back(Vec2d(r*cos(angle), r*sin(angle)));
 		}
         m_shape = points;
+        break;
     }
-#if ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
-    else if (page_type == Bed3D::EShapeType::Custom)
-#else
-    else if (page_type == BedShape::Type::Custom)
-#endif // ENABLE_OUT_OF_BED_DETECTION_IMPROVEMENTS
+    case BedShape::PageType::Custom:
         m_shape = m_loaded_shape;
+        break;
+    }
 
     update_preview();
 }

src/libslic3r/BoundingBox.hpp
@@ -111,7 +111,7 @@ public:
     void translate(coordf_t x, coordf_t y, coordf_t z) { assert(this->defined); PointClass v(x, y, z); this->min += v; this->max += v; }
     void translate(const Vec3d &v) { this->min += v; this->max += v; }
     void offset(coordf_t delta);
-    BoundingBoxBase<PointClass> inflated(coordf_t delta) const throw() { BoundingBoxBase<PointClass> out(*this); out.offset(delta); return out; }
+    BoundingBox3Base<PointClass> inflated(coordf_t delta) const throw() { BoundingBox3Base<PointClass> out(*this); out.offset(delta); return out; }
     PointClass center() const;
     coordf_t max_size() const;
 

src/libslic3r/BuildVolume.hpp
@@ -1,0 +1,120 @@
+#ifndef slic3r_BuildVolume_hpp_
+#define slic3r_BuildVolume_hpp_
+
+#include "Point.hpp"
+#include "Geometry/Circle.hpp"
+
+#include <string_view>
+
+namespace Slic3r {
+
+struct GCodeProcessorResult;
+
+// For collision detection of objects and G-code (extrusion paths) against the build volume.
+class BuildVolume
+{
+public:
+    enum class Type : unsigned char
+    {
+        // Not set yet or undefined.
+        Invalid,
+        // Rectangular print bed. Most common, cheap to work with.
+        Rectangle,
+        // Circular print bed. Common on detals, cheap to work with.
+        Circle,
+        // Convex print bed. Complex to process.
+        Convex,
+        // Some non convex shape.
+        Custom
+    };
+
+    // Initialized to empty, all zeros, Invalid.
+    BuildVolume() {}
+    // Initialize from PrintConfig::bed_shape and PrintConfig::max_print_height
+    BuildVolume(const std::vector<Vec2d> &bed_shape, const double max_print_height);
+
+    // Source data, unscaled coordinates.
+    const std::vector<Vec2d>&   bed_shape()         const { return m_bed_shape; }
+    double                      max_print_height()  const { return m_max_print_height; }
+    
+    // Derived data
+    Type                        type()              const { return m_type; }
+    // Format the type for console output.
+    static std::string_view     type_name(Type type);
+    std::string_view            type_name()         const { return type_name(m_type); }
+    bool                        valid()             const { return m_type != Type::Invalid; }
+    // Same as bed_shape(), but scaled coordinates.
+    const Polygon&              polygon()           const { return m_polygon; }
+    // Bounding box of polygon(), scaled.
+    const BoundingBox&          bounding_box()      const { return m_bbox; }
+    // Bounding volume of bed_shape(), max_print_height(), unscaled.
+    const BoundingBoxf3&        bounding_volume()   const { return m_bboxf; }
+    BoundingBoxf                bounding_volume2d() const { return { to_2d(m_bboxf.min), to_2d(m_bboxf.max) }; };
+
+    // Center of the print bed, unscaled.
+    Vec2d                       bed_center()        const { return to_2d(m_bboxf.center()); }
+    // Convex hull of polygon(), scaled.
+    const Polygon&              convex_hull()       const { return m_convex_hull; }
+    // Smallest enclosing circle of polygon(), scaled.
+    const Geometry::Circled&    circle()            const { return m_circle; }
+
+    enum class ObjectState : unsigned char
+    {
+        // Inside the build volume, thus printable.
+        Inside,
+        // Colliding with the build volume boundary, thus not printable and error is shown.
+        Colliding,
+        // Outside of the build volume means the object is ignored: Not printed and no error is shown.
+        Outside
+    };
+
+    // 1) Tests called on the plater.
+    // Using SceneEpsilon for all tests.
+    static constexpr const double SceneEpsilon = EPSILON;
+    // Called by Plater to update Inside / Colliding / Outside state of ModelObjects before slicing.
+    // Called from Model::update_print_volume_state() -> ModelObject::update_instances_print_volume_state()
+    // Using SceneEpsilon
+    ObjectState  object_state(const indexed_triangle_set &its, const Transform3f &trafo, bool may_be_below_bed) const;
+    // Called by GLVolumeCollection::check_outside_state() after an object is manipulated with gizmos for example.
+    // Called for a rectangular bed:
+    ObjectState  volume_state_bbox(const BoundingBoxf3 &volume_bbox) const;
+
+    // 2) Test called on G-code paths.
+    // Using BedEpsilon for all tests.
+    static constexpr const double BedEpsilon = 3. * EPSILON;
+    // Called on final G-code paths.
+    //FIXME The test does not take the thickness of the extrudates into account!
+    bool         all_paths_inside(const GCodeProcessorResult &paths, const BoundingBoxf3 &paths_bbox) const;
+    // Called on initial G-code preview on OpenGL vertex buffer interleaved normals and vertices.
+    bool         all_paths_inside_vertices_and_normals_interleaved(const std::vector<float> &paths, const Eigen::AlignedBox<float, 3> &bbox) const;
+
+private:
+    // Source definition of the print bed geometry (PrintConfig::bed_shape)
+    std::vector<Vec2d>  m_bed_shape;
+    // Source definition of the print volume height (PrintConfig::max_print_height)
+    double              m_max_print_height;
+
+    // Derived values.
+    Type                m_type { Type::Invalid };
+    // Geometry of the print bed, scaled copy of m_bed_shape.
+    Polygon             m_polygon;
+    // Scaled snug bounding box around m_polygon.
+    BoundingBox         m_bbox;
+    // 3D bounding box around m_shape, m_max_print_height.
+    BoundingBoxf3       m_bboxf;
+    // Area of m_polygon, scaled.
+    double              m_area { 0. };
+    // Convex hull of m_polygon, scaled.
+    Polygon             m_convex_hull;
+    // For collision detection against a convex build volume. Only filled in for m_type == Convex or Custom.
+    // Variant with SceneEpsilon applied.
+    std::pair<std::vector<Vec2d>, std::vector<Vec2d>>   m_top_bottom_convex_hull_decomposition_scene;
+    // Variant with BedEpsilon applied.
+    std::pair<std::vector<Vec2d>, std::vector<Vec2d>>   m_top_bottom_convex_hull_decomposition_bed;
+    // Smallest enclosing circle of m_polygon, scaled.
+    Geometry::Circled   m_circle { Vec2d::Zero(), 0 };
+};
+
+} // namespace Slic3r
+
+#endif // slic3r_BuildVolume_hpp_

src/libslic3r/CMakeLists.txt
@@ -23,6 +23,8 @@ add_library(libslic3r STATIC
     BridgeDetector.hpp
     Brim.cpp
     Brim.hpp
+    BuildVolume.cpp
+    BuildVolume.hpp
     clipper.cpp
     clipper.hpp
     ClipperUtils.cpp
@@ -116,6 +118,8 @@ add_library(libslic3r STATIC
     Geometry.hpp
     Geometry/Circle.cpp
     Geometry/Circle.hpp
+    Geometry/ConvexHull.cpp
+    Geometry/ConvexHull.hpp
     Geometry/MedialAxis.cpp
     Geometry/MedialAxis.hpp
     Geometry/Voronoi.hpp

src/libslic3r/ClipperUtils.cpp
@@ -542,6 +542,8 @@ static inline Polygons _clipper(ClipperLib::ClipType clipType, TSubj &&subject, 
     return to_polygons(clipper_do<ClipperLib::Paths>(clipType, std::forward<TSubj>(subject), std::forward<TClip>(clip), ClipperLib::pftNonZero, do_safety_offset));
 }
 
+Slic3r::Polygons diff(const Slic3r::Polygon &subject, const Slic3r::Polygon &clip, ApplySafetyOffset do_safety_offset)
+    { return _clipper(ClipperLib::ctDifference, ClipperUtils::SinglePathProvider(subject.points), ClipperUtils::SinglePathProvider(clip.points), do_safety_offset); }
 Slic3r::Polygons diff(const Slic3r::Polygons &subject, const Slic3r::Polygons &clip, ApplySafetyOffset do_safety_offset)
     { return _clipper(ClipperLib::ctDifference, ClipperUtils::PolygonsProvider(subject), ClipperUtils::PolygonsProvider(clip), do_safety_offset); }
 Slic3r::Polygons diff(const Slic3r::Polygons &subject, const Slic3r::ExPolygons &clip, ApplySafetyOffset do_safety_offset)

src/libslic3r/ClipperUtils.hpp
@@ -331,6 +331,8 @@ Slic3r::ExPolygons union_safety_offset_ex(const Slic3r::Polygons &polygons);
 Slic3r::ExPolygons union_safety_offset_ex(const Slic3r::ExPolygons &expolygons);
 
 // Aliases for the various offset(...) functions, conveying the purpose of the offset.
+inline Slic3r::Polygons   expand(const Slic3r::Polygon &polygon, const float delta, ClipperLib::JoinType joinType = DefaultJoinType, double miterLimit = DefaultMiterLimit) 
+    { assert(delta > 0); return offset(polygon, delta, joinType, miterLimit); }
 inline Slic3r::Polygons   expand(const Slic3r::Polygons &polygons, const float delta, ClipperLib::JoinType joinType = DefaultJoinType, double miterLimit = DefaultMiterLimit) 
     { assert(delta > 0); return offset(polygons, delta, joinType, miterLimit); }
 inline Slic3r::ExPolygons expand_ex(const Slic3r::Polygons &polygons, const float delta, ClipperLib::JoinType joinType = DefaultJoinType, double miterLimit = DefaultMiterLimit) 
@@ -378,6 +380,7 @@ inline Slic3r::ExPolygons opening_ex(const Slic3r::Surfaces &surfaces, const flo
 Slic3r::Lines _clipper_ln(ClipperLib::ClipType clipType, const Slic3r::Lines &subject, const Slic3r::Polygons &clip);
 
 // Safety offset is applied to the clipping polygons only.
+Slic3r::Polygons   diff(const Slic3r::Polygon &subject, const Slic3r::Polygon &clip, ApplySafetyOffset do_safety_offset = ApplySafetyOffset::No);
 Slic3r::Polygons   diff(const Slic3r::Polygons &subject, const Slic3r::Polygons &clip, ApplySafetyOffset do_safety_offset = ApplySafetyOffset::No);
 Slic3r::Polygons   diff(const Slic3r::Polygons &subject, const Slic3r::ExPolygons &clip, ApplySafetyOffset do_safety_offset = ApplySafetyOffset::No);
 Slic3r::Polygons   diff(const Slic3r::ExPolygons &subject, const Slic3r::Polygons &clip, ApplySafetyOffset do_safety_offset = ApplySafetyOffset::No);

src/libslic3r/ExPolygonCollection.cpp
@@ -1,5 +1,5 @@
 #include "ExPolygonCollection.hpp"
-#include "Geometry.hpp"
+#include "Geometry/ConvexHull.hpp"
 
 namespace Slic3r {
 

commit f32e7b3281e017c861ebeac2f3a4d98e47c7d25f

Fix of Bed shape inverted origin position #7474 The bug was introduced with 543b722 due to out of print bed detection refactoring.

src/slic3r/GUI/BedShapeDialog.cpp
@@ -109,7 +109,7 @@ wxString BedShape::get_full_name_with_params()
     default:
         // rectangle, convex, concave...
         out += "\n" + _(get_option_label(Parameter::RectSize)) + ": [" + ConfigOptionPoint(to_2d(m_build_volume.bounding_volume().size())).serialize() + "]";
-        out += "\n" + _(get_option_label(Parameter::RectOrigin)) + ": [" + ConfigOptionPoint(to_2d(m_build_volume.bounding_volume().min)).serialize() + "]";
+        out += "\n" + _(get_option_label(Parameter::RectOrigin)) + ": [" + ConfigOptionPoint(- to_2d(m_build_volume.bounding_volume().min)).serialize() + "]";
         break;
     }
     return out;
@@ -124,7 +124,7 @@ void BedShape::apply_optgroup_values(ConfigOptionsGroupShp optgroup)
     default:
         // rectangle, convex, concave...
         optgroup->set_value("rect_size"     , new ConfigOptionPoints{ to_2d(m_build_volume.bounding_volume().size()) });
-        optgroup->set_value("rect_origin"   , new ConfigOptionPoints{ to_2d(m_build_volume.bounding_volume().min) });
+        optgroup->set_value("rect_origin"   , new ConfigOptionPoints{ - to_2d(m_build_volume.bounding_volume().min) });
     }
 }
 